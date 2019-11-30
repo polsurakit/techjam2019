1c385aa10f444b9db6dbbbc9a0aa546088ca1c33
ekkalak <ekkalak.leelasornchai@scb.co.th>
2019-11-30 16:22:51 +0700

    requirement B

--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,18 @@
-var express = require('express');
-var router = express.Router();
-const service = require('../service');
+var express = require('express')
+var router = express.Router()
+const service = require('../service')
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+  res.send('respond with a resource')
+})
 
 router.post('/distance', function(req, res, next) {
-  const p1 = req.body.first_pos;
-  const p2 = req.body.second_pos;
-  res.send(service.util.distance(p1,p2));
-} )
+  const { first_pos, second_pos, metric } = req.body
+  try {
+    res.send(service.util.distance(first_pos, second_pos, metric))
+  } catch (e) {
+    res.status(400).send({ error: 'Bad Request' })
+  }
+})
 
-module.exports = router;
+module.exports = router
--- service/util.js
+++ service/util.js
@@ -1,7 +1,15 @@
-function distance(p1, p2) {
-  return { distance: Math.sqrt((p1.x-p2.x)*(p1.x-p2.x) + (p1.y-p2.y)*(p1.y-p2.y)) };
+function distance(p1, p2, metric = undefined) {
+  if (metric === 'manhattan') {
+    return { distance: Math.abs(p1.x - p2.x) + Math.abs(p1.y - p2.y) }
+  } else {
+    return {
+      distance: Math.sqrt(
+        (p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y)
+      )
+    }
+  }
 }
 
 module.exports = {
-  distance,
+  distance
 }
