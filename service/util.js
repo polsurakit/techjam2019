39c0115ca00974deed83128c97ef00e00816d321
polsu <pol.su@easysunday.com>
2019-11-30 16:04:27 +0700

    base feature

--- service/util.js
+++ service/util.js
@@ -1,5 +1,5 @@
 function distance(p1, p2) {
-  return { distance: sqrt((p1.x-p2.x)*(p1.x-p2.x) + (p1.y-p2.y)*(p1.y-p2.y)) };
+  return { distance: Math.sqrt((p1.x-p2.x)*(p1.x-p2.x) + (p1.y-p2.y)*(p1.y-p2.y)) };
 }
 
 module.exports = {
