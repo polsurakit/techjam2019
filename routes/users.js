42c81af5b695665b5f0d4921fb43d2a1aeafb868
polsu <pol.su@easysunday.com>
2019-11-30 16:19:36 +0700

    feature A

--- routes/users.js
+++ routes/users.js
@@ -10,6 +10,13 @@ router.post('/distance', function(req, res, next) {
   const p1 = req.body.first_pos;
   const p2 = req.body.second_pos;
   res.send(service.util.distance(p1,p2));
-} )
+});
+
+router.put('/robot/:robotId/position', function(req, res, next) {
+  const robotId = req.params.robotId;
+  const position = req.body.position;
+  service.robot.setPosition(robotId, position);
+  res.status(204).end();
+});
 
 module.exports = router;
